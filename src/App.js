26e375c14cf440e863db546ad623b9bbc595b794
Danielle <136658130+Smashhedo@users.noreply.github.com>
2023-07-28 14:54:59 -0500

    setData

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Buttons from "./components/Buttons";
 import Table from "./components/Table";
 import Banner from "./components/Banner";
 import useState from "react";
+import satData from "./components/satData";
 
 
 function App() {
