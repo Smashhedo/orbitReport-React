7d2de20ec289f4339c12febd6db3b522c3c8b8c4
Danielle <136658130+Smashhedo@users.noreply.github.com>
2023-07-28 14:28:22 -0500

    stupid curly braces

--- src/App.js
+++ src/App.js
@@ -8,10 +8,8 @@ import satData from "./components/satData";
 function App() {
   const [sat, setSat] = useState(satData);
   const displaySats = [...new Set(satData.map((data) => data.orbitType))];
-    
-}
-    const filterByType = (currentType) => {
-    const displaySats = satData.filter((newSatDisplay) => {
+  const filterByType = (currentType) => {
+  const displaySats = satData.filter((newSatDisplay) => {
      return newSatDisplay.orbitType === currentType;
     });
     setSat(displaySats);
@@ -25,6 +23,8 @@ function App() {
       <Table sat={sat} />
       <Banner />
     </div>
-  )};
+
+  )}
+};
 
 export default App;
