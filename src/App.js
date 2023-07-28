cf1fa18ed52e8809a336e5b9fa53a367fd556100
Danielle <136658130+Smashhedo@users.noreply.github.com>
2023-07-28 14:44:42 -0500

    passing props into button

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import Buttons from "./components/Buttons";
 import Table from "./components/Table";
 import Banner from "./components/Banner";
 import useState from "react";
-import satData from "./components/satData";
 
 
 function App() {
